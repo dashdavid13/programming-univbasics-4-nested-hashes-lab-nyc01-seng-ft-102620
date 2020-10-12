35a43848cc6658ec34fbbaebc8cf506c1cc4ee10
David Medina <davidamedina619@gmail.com>
2020-10-12 06:32:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -145,6 +145,6 @@ def adding_to_dennis
 
 end
 
-programmer_hash[:dennis_ritchie][:languages] = "Assembly"
-programmer_hash
-
+another_hash = {:item1 => 1}
+another_hash = {:item2 => 2, :item3 => 3}
+programmer_hash[:dennis_ritchie][:languages].merge(another_hash)
